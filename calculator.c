0c362816d8183ed881133252a0f1d311f1913e7e
danielandricic <dani.andricic@gmail.com>
2018-12-15 22:40:31 +0100

    Calculator finished

---
+++ calculator.c
@@ -0,0 +1,110 @@
+#include "calculator.h"
+#include <stdio.h>
+#include <float.h>
+
+int main()
+{
+  int option = 0;
+  double result = 0;
+  printf("Calculator\n==========\n");
+  printoption();
+  result = getinputandcalculate(&option);
+  if(option != -1)
+  {
+    printf("\nDas Ergebnis beträgt %.1f.\n", result);
+  }
+  return 0;
+}
+
+void printoption()
+{
+  printf("\nChoose one of the following operations:\n");
+  printf("Add (1)\nSubtract (2)\nMultiply (3)\nDivide (4)\nStop program (-1)\n");
+  printf("Enter your choice: ");
+}
+
+double getinputandcalculate(int *option)
+{
+  double firstnumber = 0;
+  double secondnumber = 0;
+
+  scanf("%d", option);
+
+  while(*option == 0 || *option < -1 || *option > 4)
+  {
+    printf("\nOption not allowed, please try again.\n");
+    scanf("%d", option);
+  }
+
+  printf("\nBitte geben Sie die erste Zahl ein: ");
+  scanf("%lf", &firstnumber);
+  checkfornumber(&firstnumber);
+
+  printf("\nBitte geben Sie die zweite Zahl ein: ");
+  scanf("%lf", &secondnumber);
+  checkfornumber(&secondnumber);
+
+  switch(*option)
+  {
+    case 1:
+      return Add(firstnumber, secondnumber);
+    break;
+    case 2:
+      return Subtract(firstnumber, secondnumber);
+    break;
+    case 3:
+      return Multiply(firstnumber, secondnumber);
+    break;
+    case 4:
+      return Divide(firstnumber, secondnumber);
+    break;
+    default:
+      return 0;
+    break;
+  }
+}
+
+double Add(double firstnumber, double secondnumber){
+  return firstnumber + secondnumber;
+}
+
+double Subtract(double firstnumber, double secondnumber)
+{
+  return firstnumber - secondnumber;
+}
+
+double Multiply(double firstnumber, double secondnumber)
+{
+  return firstnumber * secondnumber;
+}
+
+double Divide(double firstnumber, double secondnumber)
+{
+  while(secondnumber != 0)
+  {
+    printf("Dividieren durch 0 ist verboten!\n Zeite Zahl neu eingeben: ");
+    scanf("%lf", &secondnumber);
+  }
+
+  return firstnumber / secondnumber;
+}
+
+void checkfornumber(double *number)
+{
+  while(1){
+    if(*number > DBL_MAX)
+    {
+      printf("\nNumber overflow!\n Enter a new number: ");
+      scanf("%lf", number);
+    }
+    else if(*number < DBL_MIN)
+    {
+      printf("\nNumber underflow!\n Enter a new number: ");
+      scanf("%lf", number);
+    }
+    else
+    {
+      return;
+    }
+  }
+}
---
+++ calculator.h
@@ -0,0 +1,11 @@
+#ifndef ___CALCULATOR__H
+#define ___CALCULATOR__H
+
+void printoption();
+double getinputandcalculate(int *option);
+double Add(double firstnumber, double secondnumber);
+double Subtract(double firstnumber, double secondnumber);
+double Multiply(double firstnumber, double secondnumber);
+double Divide(double firstnumber, double secondnumber);
+void checkfornumber(double *number);
+#endif
